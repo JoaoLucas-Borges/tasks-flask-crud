78b51d7de94fd9c8816aa208521022c8391122ad
JoaoLucas-Borges <joaocavalcantt@outlook.com>
2026-07-15 16:31:28 -0300

    tests

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "New task created"}) # jsonify formata para JSON, padrão REST
+    return jsonify({"message": "New task created", "id": new_task.id}) # jsonify formata para JSON, padrão REST
 
 
 @app.route("/tasks", methods=["GET"]) # Read
